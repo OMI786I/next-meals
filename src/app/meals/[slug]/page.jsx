1b537979fac0b018bb9812136374775e85f721ac
OMI786I <sohrawardy1998@gmail.com>
2024-09-22 00:12:38 +0600

    first commit

---
+++ src/app/meals/[slug]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>This is dynamic</div>;
+};
+
+export default page;
---
+++ src/app/meals/share/page.jsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return <div>Meals share</div>;
+};
+
+export default page;
