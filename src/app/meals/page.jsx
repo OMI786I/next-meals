38fdae3e7f74296b9e932cbde5f79d884fb2a3a1
OMI786I <sohrawardy1998@gmail.com>
2024-09-22 19:31:20 +0600

    populating the starting page content

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,18 +1,3 @@
-import Link from "next/link";
-import React from "react";
-
-const page = () => {
-  return (
-    <div>
-      This is meals page
-      <p>
-        <Link href={"/meals/share"}>Share</Link>
-      </p>
-      <p>
-        <Link href={"/meals/meals-1"}>Meals 1 (dynamic)</Link>
-      </p>
-    </div>
-  );
-};
-
-export default page;
+export default function Home() {
+  return <div>meals</div>;
+}
